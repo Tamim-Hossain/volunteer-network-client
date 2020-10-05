c9f560a31169e1c045bf159921bb12233b93695f
Tamim <tamim-hossain>
2020-10-05 16:27:11 +0600

    updated

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import './Event.css'
 
 const Event = ({ event: { name, img } }) => {
     return (
-        <Card className="col-md-2 text-center m-3 p-2" >
+        <Card className="col-md-2 text-center m-3 p-2 border-0 shadow p-3 mb-5 bg-white rounded" style={{ cursor: 'pointer' }} >
             <Link className="text-decoration-none" to={`/event/${name}`}>
                 <Card.Img variant="top" src={img} />
                 <Card.Body>
                     <Card.Title>{name}</Card.Title>
                 </Card.Body>
             </Link>
-        </Card>
+        </Card >
     );
 };
 
