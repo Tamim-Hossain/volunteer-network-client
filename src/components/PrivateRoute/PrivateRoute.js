58b1306419993ca430166a3f4ac9960d7fe4fff2
Tamim-Hossain <tamim_hossain@outlook.com>
2021-02-16 14:35:16 +0600

    private route fixed

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,26 +1,27 @@
-import React, { useContext } from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { UserContext } from '../../App';
+import React, { useContext } from "react";
+import { Redirect, Route } from "react-router-dom";
+import { UserContext } from "../../App";
 
 function PrivateRoute({ children, ...rest }) {
-    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-    return (
-        <Route
-            {...rest}
-            render={({ location }) =>
-                loggedInUser.email ? (
-                    children
-                ) : (
-                        <Redirect
-                            to={{
-                                pathname: "/login",
-                                state: { from: location }
-                            }}
-                        />
-                    )
-            }
-        />
-    );
+	const [loggedInUser] = useContext(UserContext);
+	const { email } = loggedInUser;
+	return (
+		<Route
+			{...rest}
+			render={({ location }) =>
+				email ? (
+					children
+				) : (
+					<Redirect
+						to={{
+							pathname: "/login",
+							state: { from: location },
+						}}
+					/>
+				)
+			}
+		/>
+	);
 }
 
 export default PrivateRoute;
